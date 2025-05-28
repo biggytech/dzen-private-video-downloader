35541aabd8391e69d04edc31f4096d4403cfdb99
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-05-28 23:51:53 +0300

    feat(videos): open video page

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import os.path
 # Constants
 URL = 'https://dzen.ru/'
 COOKIES_FILE = "cookies.pkl"
+EXIT_KEYWORD = "exit"
+
+# CSS selectors
+USER_PROFILE_BUTTON = "button[class*=\"dzen-layout--avatar__isButton\"]"
+VIDEO_PLAYER = "[class*=\"player__videoViewerPlayer\"]"
 
 print("Opening " + URL + "...")
 
@@ -26,8 +31,8 @@ print("Please, authenticate in " + URL + " for further work")
 print("Waiting for user authentication...")
 
 # Now wait for me to manually load a page, and look for the CSS selector.
-user_profile_button = WebDriverWait(driver, 300).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "button[class*=\"dzen-layout--avatar__isButton\"]"))
+WebDriverWait(driver, 300).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, USER_PROFILE_BUTTON))
 )
 
 print("User successfully authenticated!")
@@ -35,6 +40,25 @@ print("User successfully authenticated!")
 # Store browser cookies
 pickle.dump( driver.get_cookies() , open(COOKIES_FILE,"wb"))
 
+# Request URLs
+while True:
+    user_input = input("Paste video URL (enter 'exit' to exit): ")
+    if user_input == EXIT_KEYWORD:
+        break
+
+    # do the stuff
+    url = user_input
+    driver.get(url)
+
+    print("Waiting for video page to load...")
+
+    # wait till opens
+    WebDriverWait(driver, 300).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, VIDEO_PLAYER))
+    )
+
 # # Once the selector is found, it will grab the `id` attribute and print it
 # text_input = element.get_attribute("id")
 # print(text_input)
+
+# window.YandexZen.VideoPlayerBundle.videoPlayersManager.players[0].convertedStreams.MPEG['Invariant quality']
